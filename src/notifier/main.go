5ac7b240e9f601b22fc9ce99d1d81aef2f5d342b
ayman <enkhalifapro@gmail.com>
2018-02-22 15:52:23 +0200

    initial commit

---
+++ src/notifier/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/TIBCOSoftware/flogo-lib/app"
+	"github.com/TIBCOSoftware/flogo-lib/engine"
+	"github.com/TIBCOSoftware/flogo-lib/logger"
+)
+
+var (
+	cp app.ConfigProvider
+)
+
+func main() {
+
+	if cp == nil {
+		// Use default config provider
+		cp = app.DefaultConfigProvider()
+	}
+
+	app, err := cp.GetApp()
+	if err != nil {
+        	fmt.Println(err.Error())
+        	os.Exit(1)
+    	}
+
+    	e, err := engine.New(app)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	e.Start()
+
+	exitChan := setupSignalHandling()
+
+	code := <-exitChan
+
+	e.Stop()
+
+	os.Exit(code)
+}
+
+func setupSignalHandling() chan int {
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan,
+		syscall.SIGHUP,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+		syscall.SIGQUIT)
+
+	exitChan := make(chan int)
+	go func() {
+		for {
+			s := <-signalChan
+			switch s {
+			// kill -SIGHUP
+			case syscall.SIGHUP:
+				exitChan <- 0
+			// kill -SIGINT/Ctrl+c
+			case syscall.SIGINT:
+				exitChan <- 0
+			// kill -SIGTERM
+			case syscall.SIGTERM:
+				exitChan <- 0
+			// kill -SIGQUIT
+			case syscall.SIGQUIT:
+				exitChan <- 0
+			default:
+				logger.Debug("Unknown signal.")
+				exitChan <- 1
+			}
+		}
+	}()
+
+	return exitChan
+}
