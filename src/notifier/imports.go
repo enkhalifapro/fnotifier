89f6132f57b1e5dde555b5a12aec611fff272d58
ayman <enkhalifapro@gmail.com>
2018-02-22 17:38:46 +0200

    deploy

--- src/notifier/imports.go
+++ src/notifier/imports.go
@@ -4,5 +4,4 @@ import (
 	_ "github.com/TIBCOSoftware/flogo-contrib/action/flow"
 	_ "github.com/TIBCOSoftware/flogo-contrib/activity/log"
 	_ "github.com/TIBCOSoftware/flogo-contrib/trigger/rest"
-	_ "github.com/enkhalifapro/fnotifier/activity/sendemail"
 )
